b63984a619e1002d78b01d0730d2d576a53809ee
Andrii Ryzhkov <ryzhkov.andrii@gmail.com>
2017-07-30 11:58:13 +0300

    MapMapper refactored

--- src/main/java/mapper/MapMapper.java
+++ src/main/java/mapper/MapMapper.java
@@ -1,6 +1,5 @@
 package main.java.mapper;
 
-import main.java.JsonSerializer;
 import main.java.writer.IJsonWriter;
 
 import java.util.Map;
@@ -10,32 +9,17 @@ public class MapMapper extends IJsonMapper {
 
     @Override
     public void write(Object obj, IJsonWriter writer) {
-        if (obj == null) {
-            writer.writeNull();
-        } else {
-            Map map = (Map) obj;
-            writer.writeObjectBegin();
-            if (map.isEmpty()) {
-                writer.writeObjectEnd();
-            } else {
-                Set mapSet = map.entrySet();
-                Map.Entry entry = (Map.Entry) mapSet.iterator().next();
-                Class keyClazz = entry.getKey().getClass();
-                Class valueClazz = entry.getValue().getClass();
-                IJsonMapper keyMapper = jsonSerializer.getMapper(keyClazz);
-                IJsonMapper valueMapper = jsonSerializer.getMapper(valueClazz);
-
-                for (Object entryElement : map.entrySet()) {
-                    Map.Entry entryMap = (Map.Entry) entryElement;
-                    keyMapper.write(entryMap.getKey(), writer);
-                    writer.writePropertySeparator();
-                    valueMapper.write(entryMap.getValue(), writer);
-                    writer.writeSeparator();
-                }
-
-                writer.writeObjectEnd();
-            }
+        Map map = (Map) obj;
+        writer.writeArrayBegin();
+        if (!map.isEmpty()) {
+            Set<Map.Entry> entrySet = map.entrySet();
+            entrySet.stream().forEach(item -> {
+                jsonSerializer.serialize(item.getKey(), writer);
+                writer.writePropertySeparator();
+                jsonSerializer.serialize(item.getValue(), writer);
+                writer.writeSeparator();
+            });
         }
-        writer.flush();
+        writer.writeArrayEnd();
     }
 }
