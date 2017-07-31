a4c1ffa6d9c31af2de210e35c32d4d8f4a5910ce
Andrii Ryzhkov <ryzhkov.andrii@gmail.com>
2017-07-31 10:06:27 +0300

    getFieldList() method refactored

--- src/main/java/mapper/POJOMapper.java
+++ src/main/java/mapper/POJOMapper.java
@@ -18,45 +18,17 @@ public class POJOMapper extends IJsonMapper {
     }
 
     private List<Field> getFieldsList(Class clazz) {
-        List<Field> fields = Arrays.stream(clazz.getDeclaredFields())
-                .filter(this::isSerialising)
-                .collect(Collectors.toList());
-        fields = filterFieldListByNameFromAnnotation(fields);
-        return fields;
+        Set<String> namesSet = new HashSet<>();
+        return Arrays.stream(clazz.getDeclaredFields())
+                     .filter(this::isSerialising)
+                     .filter(field -> namesSet.add((hasJsonProperty(field))
+                             ? getAnnotationName(field)
+                             : field.getName()))
+                     .collect(Collectors.toList());
     }
 
-    private List<Field> filterTheSameAnnotationName(List<Field> fields) {
-        Set<String> annotationNamesList = new HashSet<>();
-        Iterator<Field> iterator = fields.iterator();
-        while (iterator.hasNext()) {
-            Field field = iterator.next();
-            if (hasJsonProperty(field)) {
-                String annotationName = field.getAnnotation(JsonProperty.class).value();
-                if (annotationNamesList.contains(annotationName)) {
-                    iterator.remove();
-                } else {
-                    annotationNamesList.add(annotationName);
-                }
-            }
-        }
-        return fields;
-    }
-
-    private List<Field> filterFieldListByNameFromAnnotation(List<Field> fields) {
-        fields = filterTheSameAnnotationName(fields);
-        Set<String> fieldsNamesFromAnnotations = new HashSet<>();
-        for (Field field : fields) {
-            if (hasJsonProperty(field)) {
-                String annotationName = field.getAnnotation(JsonProperty.class).value();
-                if (!annotationName.equals(field.getName())) {
-                    fieldsNamesFromAnnotations.add(field.getAnnotation(JsonProperty.class).value());
-                }
-            }
-        }
-        fields = fields.stream()
-                .filter(field -> !fieldsNamesFromAnnotations.contains(field.getName()))
-                .collect(Collectors.toList());
-        return fields;
+    private String getAnnotationName(Field field) {
+        return field.getAnnotation(JsonProperty.class).name();
     }
 
     private boolean isPublic(Field field) {
@@ -71,7 +43,9 @@ public class POJOMapper extends IJsonMapper {
         boolean isNotTransient = !Modifier.isTransient(field.getModifiers());
         boolean isNotStatic = !Modifier.isStatic(field.getModifiers());
         boolean hasNotJsonIgnore = !field.isAnnotationPresent(JsonIgnore.class);
-        return isNotStatic && ((isPublic(field) && isNotTransient) || hasJsonProperty(field)) && hasNotJsonIgnore;
+        return isNotStatic
+                && ((isPublic(field) && isNotTransient) || hasJsonProperty(field))
+                && hasNotJsonIgnore;
     }
 
     public void write(Object obj, IJsonWriter jsonWriter) {
@@ -80,7 +54,9 @@ public class POJOMapper extends IJsonMapper {
         } else {
             jsonWriter.writeObjectBegin();
             for (Field field : fieldsList) {
-                String name = hasJsonProperty(field) ? field.getAnnotation(JsonProperty.class).value() : field.getName();
+                String name = hasJsonProperty(field)
+                        ? getAnnotationName(field)
+                        : field.getName();
                 jsonWriter.writeString(name);
                 jsonWriter.writePropertySeparator();
                 try {
