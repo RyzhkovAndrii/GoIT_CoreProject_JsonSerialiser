0fc9a23810356930d223f4ee336a1dbe6f2d1ecf
Andrii Ryzhkov <ryzhkov.andrii@gmail.com>
2017-08-02 23:36:45 +0300

    comments deleted

--- src/main/java/goit/gojava7/group7/jsonserialiser/writer/IJsonWriter.java
+++ src/main/java/goit/gojava7/group7/jsonserialiser/writer/IJsonWriter.java
@@ -2,72 +2,26 @@ package goit.gojava7.group7.jsonserialiser.writer;
 
 public interface IJsonWriter {
 
-    /**
-     * Add the begin of Object ({) in buffer
-     */
     void writeObjectBegin();
 
-    /**
-     * Add the end of Object (}) in buffer.
-     * If the previous symbol is comma (,) then delete it.
-     */
     void writeObjectEnd();
 
-    /**
-     * Add the begin of Array ([) into buffer
-     */
     void writeArrayBegin();
 
-    /**
-     * Add the end of Array (]) into buffer.
-     * If the previous symbol is comma (,) then delete it.
-     */
     void writeArrayEnd();
 
-    /**
-     * Add double quotes (") to begin and end of the string, if string has
-     * double quotes (") in start and end, then add slash (/) to thees
-     * double quotes ("), and then add this string into buffer.
-     *
-     * @param string is adding String
-     * @throws NullPointerException if string is null
-     */
     void writeString(String string) throws NullPointerException;
 
-    /**
-     * Add number into buffer
-     *
-     * @param number is adding Number
-     * @throws NullPointerException if number is null
-     */
     void writeNumber(Number number) throws NullPointerException;
 
-    /**
-     * Add comma (,) into buffer
-     */
     void writeSeparator();
 
-    /**
-     * Add colon (:) into buffer
-     */
     void writePropertySeparator();
 
-    /**
-     * Add true or false into buffer
-     *
-     * @param bool is adding Boolean
-     * @throws NullPointerException if bool is null
-     */
     void writeBoolean(Boolean bool) throws NullPointerException;
 
-    /**
-     * Add null into buffer
-     */
     void writeNull();
 
-    /**
-     * Write buffer into writer
-     */
     void flush();
 
 }
